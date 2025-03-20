1acb399e8e1d87590c6a0d4f7a3bf68c86ad6725
Sam <ww123ww@126.com>
2025-03-20 21:19:36 +0800

    add template

---
+++ template/app.js
@@ -0,0 +1,27 @@
+const app = Vue.createApp({
+	data() {
+		return {
+			yourAge: 'YOUR AGE',
+			yourAge5: " in 5 years is ",
+			favNum: 'Favorite Number: RANDOM NUMBER BETWEEN 0 AND 1',
+			yourAgeNow : 15,
+			imageUrl: "images/black-cat.jpeg",
+			name: "Magic"
+		};
+	},
+	methods: {
+		outputYourAge5() {
+			return this.yourAge + this.yourAge5
+		},
+		outputAge5YearsLater() {
+			return this.yourAge + this.yourAge5 + (this.yourAgeNow + 5)
+		},
+		outputGoal() {
+		},
+		outputRandomNum(){
+			return Math.random()
+		}
+	},
+});
+
+app.mount('#app');
