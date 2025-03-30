deb9f315be444fb9f04d267535f06212b725d988
Sam <ww123ww@126.com>
2025-03-30 16:23:01 +0800

    add set name (event)

--- vue-event/app.js
+++ vue-event/app.js
@@ -2,6 +2,7 @@ const app = Vue.createApp({
 	data() {
 		return {
 			result: 0,
+			name: 'John',
 			productList:[
 				{title: 'A Book', quantity: 1, price: 9.99},
 				{title: 'A Carpet', quantity: 2, price: 99.99},
@@ -11,12 +12,19 @@ const app = Vue.createApp({
 		};
 	},
 	methods: {
+		setName(event, lastName){
+			// event is prvided by browser by default
+			this.name = event.target.value + ' ' + lastName;
+		},
 		addOne(){
 			this.result += 1;
 		},
 		deductOne(){
-			this.result -= 1;
+			this.result--;
 		},
+		add(num){
+			this.result += num;
+		}
 	},
 });
 
