d16172e39161f6a055da5de895c628f9ade96655
Sam <ww123ww@126.com>
2025-03-30 17:55:22 +0800

    add event modifier

--- vue-event/app.js
+++ vue-event/app.js
@@ -3,6 +3,7 @@ const app = Vue.createApp({
 		return {
 			result: 0,
 			name: 'John',
+			confirmedName: '',
 			productList:[
 				{title: 'A Book', quantity: 1, price: 9.99},
 				{title: 'A Carpet', quantity: 2, price: 99.99},
@@ -12,6 +13,13 @@ const app = Vue.createApp({
 		};
 	},
 	methods: {
+		confirmedInfo(){
+			this.confirmedName = this.name;
+		},
+		submitForm(event){
+			// event.preventDefault(); // Prevent the default form submission
+			alert('Form submitted!' + 'name='+ this.name + ' email=' + this.email);
+		},
 		setName(event, lastName){
 			// event is prvided by browser by default
 			this.name = event.target.value + ' ' + lastName;
