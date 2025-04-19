0738c2cc4fe7521fa186a393f921e90d5d1888e5
Sam <ww123ww@126.com>
2025-04-19 18:24:19 +0800

    display fruits items

--- vue3-update-array/app.js
+++ vue3-update-array/app.js
@@ -2,9 +2,18 @@ const app = Vue.createApp({
 	data() {
 		return {
 			message: 'Reactivity in Action',
+			fruits: ['apple', 'banana', 'orange'],
+			newFruit: '',
 		};
 	},
 	methods: {
+		addFruit(){
+			if (this.newFruit === '') {
+				alert('Please enter a fruit name');
+				return;
+			}	
+			this.fruits.push(this.newFruit);
+		}
 	},
 	computed:{
 	},
