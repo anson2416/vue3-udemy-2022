63be48a472e09f3e7d9cdc991c45a4b0eb0800b8
Sam <ww123ww@126.com>
2025-04-19 17:45:11 +0800

    add logic for result display

--- Reactivity in Action/app.js
+++ Reactivity in Action/app.js
@@ -2,13 +2,24 @@ const app = Vue.createApp({
 	data() {
 		return {
 			title: 'Reactivity in Action',
-            result: 'RESULT',
+            // result: 'RESULT',
+			number: 0,
 		};
 	},
 	methods: {
-		outputGoal() {
+		add(a) {
+			this.number += a;
 		},
 	},
+	computed:{
+		result(){
+			if (this.number>5) {
+				return 'Greater than 5';
+			}else{
+				return 'Less than or equal to 5';
+			}
+		}
+	}
 });
 
 app.mount('#app');
